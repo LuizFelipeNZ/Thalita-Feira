0e787c205598fc9a36ef610dbc95bb05305e08f7
Laihnsiuyhgvdsiojkb <106272946+LuizFelipeNZ@users.noreply.github.com>
2024-08-07 10:05:24 -0300

    fafa

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 alert('Boas-vindas ao jogo do número secreto'); 
-let numeroSecreto = 29;
+let numeroSecreto = 13;
 let chute=prompt('Escolha um número entre 1 e 30');
 // se chute for igual ao número secreto
 if(chute == numeroSecreto) {
-    console.log('Isso aí! Você descobriu o número secreto (29)');
+    console.log('Isso aí! Você descobriu o número secreto (13)');
 
 } else {
     alert ('Você errou :-(')
