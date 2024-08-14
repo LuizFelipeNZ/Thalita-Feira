60978d7f5d3dc9d65b028f90741ae9826793e3e2
Laihnsiuyhgvdsiojkb <106272946+LuizFelipeNZ@users.noreply.github.com>
2024-08-14 08:22:40 -0300

    fafanfkjajk

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ let chute
 while (chute != numeroSecreto)  {
     chute = prompt('Escolha um número entre 1 e 25');
     if (chute == numeroSecreto) {
-        alert('Isso aí! Você descobriu o número secreto {numeroSecreto}');
+        alert('Isso aí! Você descobriu o número secreto ');
     } else {
         if (chute > numeroSecreto) {
-        alert ('O numeroSecreto é menor que ${chute}');
+        alert ('O numeroSecreto é menor que ${chute}!');
         } else {
-        alert ('O numeroSecreto é maior que ${chute}');
+        alert ('O numeroSecreto é maior que ${chute}!');
         }
     }
   }
