9080d17b5c95e6c4191c19c5cc30a432bfb2e885
Laihnsiuyhgvdsiojkb <106272946+LuizFelipeNZ@users.noreply.github.com>
2024-08-14 08:06:39 -0300

    dfzv

--- app.js
+++ app.js
@@ -3,8 +3,21 @@ let numeroSecreto = 13;
 let chute=prompt('Escolha um número entre 1 e 30');
 // se chute for igual ao número secreto
 if(chute == numeroSecreto) {
-    console.log('Isso aí! Você descobriu o número secreto (13)');
+    console.log('Isso aí! Você descobriu o número secreto ${numeroSecreto}');
 
 } else {
     alert ('Você errou :-(')
 }
+while (chute != numeroSecreto)  {
+    chute = prompt('Escolha um número entre 1 e 10');
+    if (chute == numeroSecreto) {
+        alert('Isso aí! Você descobriu o número secreto ${numeroSecreto}');
+    } else {
+        if (chute > numeroSecreto) {
+        alert (`O numeroSecreto é menor que ${chute}`);
+        } else {
+        alert (`O numeroSecreto é maior que ${chute}`);
+        }
+    }
+  }
+
