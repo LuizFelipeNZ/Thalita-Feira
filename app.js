7316864c64d937f939883f2cc423c3aaeb59ffc7
Laihnsiuyhgvdsiojkb <106272946+LuizFelipeNZ@users.noreply.github.com>
2024-08-14 08:08:40 -0300

    ttuj

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
-alert('Boas-vindas ao jogo do número secreto'); 
+alert('Boas-vindas ao jogo do número secreto!'); 
 let numeroSecreto = 13;
 let chute=prompt('Escolha um número entre 1 e 30');
-// se chute for igual ao número secreto
-if(chute == numeroSecreto) {
-    console.log('Isso aí! Você descobriu o número secreto ${numeroSecreto}');
 
-} else {
-    alert ('Você errou :-(')
-}
 while (chute != numeroSecreto)  {
-    chute = prompt('Escolha um número entre 1 e 10');
+    chute = prompt('Escolha um número entre 1 e 15');
     if (chute == numeroSecreto) {
         alert('Isso aí! Você descobriu o número secreto ${numeroSecreto}');
     } else {
