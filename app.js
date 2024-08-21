bf0256da2741c392be961864a830ce3e5f7105cb
Laihnsiuyhgvdsiojkb <106272946+LuizFelipeNZ@users.noreply.github.com>
2024-08-21 07:58:38 -0300

    gagaga

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 alert('Boas-vindas ao jogo do número secreto!'); 
 let numeroSecreto = 13;
 let chute
+let tentativas = 1;
 
 while (chute != numeroSecreto)  {
     chute = prompt('Escolha um número entre 1 e 25');
     if (chute == numeroSecreto) {
-        alert("Isso aí! Você descobriu o número secreto " + numeroSecreto +"!");
+        alert("Isso aí! Você descobriu o número secreto " + numeroSecreto +" com " + tentativas + "tentativas !");
+
     } else {
         if (chute > numeroSecreto) {
         alert ("O numeroSecreto é menor que " + chute + "!");
@@ -13,5 +15,10 @@ while (chute != numeroSecreto)  {
         alert ("O numeroSecreto é maior que " + chute + "!");
         }
     }
+     //tentativas = tentativas + 1;
+
+
+ 
+     tentativas++;
   }
 
